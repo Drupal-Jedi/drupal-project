c5a5ae9bd089cd4ec22d649960fdc895471b1cd3
Kiselev Dmitry <kiselev.kala4ek@gmail.com>
2017-10-10 10:20:41 +0700

    Add helpful scripts

--- tools/scripts/after_pull.sh
+++ tools/scripts/after_pull.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Script execute all necessary commands after each pull.
-
 if [ ! -d "provision/local" ]; then
   echo "You must run it from the project root directory!"
   exit
@@ -11,9 +9,9 @@ cd provision/local
 echo "1/4 Installing new composer and npm packages. Building css."
 docker-compose exec php /bin/bash -c "cd docroot && composer install"
 echo "2/4 Importing new configuration"
-docker-compose exec php /bin/bash -c "cd docroot && drush cim -y" > /dev/null
+docker-compose exec php /bin/bash -c "cd docroot && drush cim -y"
 echo "3/4 Applying updates to current database"
-docker-compose exec php /bin/bash -c "cd docroot && drush updb --entity-updates -y" > /dev/null
+docker-compose exec php /bin/bash -c "cd docroot && drush updb --entity-updates -y"
 echo "4/4 Clearing cache"
-docker-compose exec php /bin/bash -c "cd docroot && drush cr" > /dev/null
+docker-compose exec php /bin/bash -c "cd docroot && drush cr"
 cd - > /dev/null
