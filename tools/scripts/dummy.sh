15d5fb95dd225f1f87824708a8ed4e6ac58ca516
Kiselev Dmitry <kiselev.kala4ek@gmail.com>
2017-10-11 11:26:58 +0700

    CI test

--- tools/scripts/dummy.sh
+++ tools/scripts/dummy.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+bash ./tools/scripts/before-install.sh travis
+cp provision/local/docker-runtime/settings/travis-settings.inc docroot/sites/default/travis-settings.inc
+
+echo "require DRUPAL_ROOT . '/sites/default/travis-settings.inc';" >> docroot/sites/prj-settings.inc
+sed -i 's/mariadb/127.0.0.1/g' docroot/sites/default/travis-settings.inc
+
+cd docroot
+drush site-install standard --account-name=admin --account-pass=admin --config-dir=../config --site-name=DrupalJedi -y
