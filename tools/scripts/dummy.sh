10094706235f8524f14702e6ef082a4e4df32fe5
Kiselev Dmitry <kiselev.kala4ek@gmail.com>
2017-10-11 17:28:45 +0700

    CI test

--- tools/scripts/dummy.sh
+++ tools/scripts/dummy.sh
@@ -7,4 +7,5 @@ echo "require DRUPAL_ROOT . '/sites/default/travis-settings.inc';" >> docroot/si
 sed -i 's/mariadb/127.0.0.1/g' docroot/sites/default/travis-settings.inc
 
 cd docroot
+../vendor/bin/drush sql-drop
 ../vendor/bin/drush site-install standard --account-name=admin --account-pass=admin --config-dir=../config --site-name=DrupalJedi -y
