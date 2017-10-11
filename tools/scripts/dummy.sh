3ec60359a6e10b55b315f491133f130500326277
Kiselev Dmitry <kiselev.kala4ek@gmail.com>
2017-10-11 17:57:37 +0700

    CI test

--- tools/scripts/dummy.sh
+++ tools/scripts/dummy.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-bash ./tools/scripts/before-install.sh travis
-cp provision/local/docker-runtime/settings/travis-settings.inc docroot/sites/default/travis-settings.inc
+bash ./tools/scripts/before-install.sh traves
+cp provision/local/docker-runtime/settings/traves-settings.inc docroot/sites/default/traves-settings.inc
 
-echo "require DRUPAL_ROOT . '/sites/default/travis-settings.inc';" >> docroot/sites/prj-settings.inc
-sed -i 's/mariadb/127.0.0.1/g' docroot/sites/default/travis-settings.inc
+echo "require DRUPAL_ROOT . '/sites/default/traves-settings.inc';" >> docroot/sites/prj-settings.inc
+sed -i 's/mariadb/127.0.0.1/g' docroot/sites/default/traves-settings.inc
 
 cd docroot
-../vendor/bin/drush sql-drop -y
 ../vendor/bin/drush site-install standard --account-name=admin --account-pass=admin --config-dir=../config --site-name=DrupalJedi -y
